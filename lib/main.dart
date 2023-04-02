4938ebdd5ea91e93f69ea60dc09de05c7f098a08
Francisco Oliveira <56833799+Jwinny@users.noreply.github.com>
2023-04-02 19:02:32 -0300

    Update game logic
    Creation o a new home page widget and update of the game logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,75 +6,75 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     // ignore: prefer_const_constructors
     return MaterialApp(
-        title: 'Pedra/nPapel/nTesoura',
-        home: Stack(
-          //  alignment: Alignment.center,
-          children: [
-            Container(
-              color: Colors.white,
-            ),
-            FractionallySizedBox(
-              heightFactor: 0.10,
-              widthFactor: 1,
-              child: Container(
-                color: Colors.black,
-                child: Text(
-                  textAlign: TextAlign.center,
-                  "Jokenpo",
-                  style: const TextStyle(
-                      backgroundColor: Color.fromARGB(0, 255, 255, 0),
-                      color: Colors.white,
-                      decoration: TextDecoration.none),
-                ),
-              ),
-            )
-          ],
-        ));
+      title: 'Jokenpo',
+      home: MyHomePage(title: 'Jokepo'),
+    );
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  final String title;
 
-//Logica de Jogo
-      // int count_player = 0;
-      // int count_computer = 0;
-      // String win = "You win, but i will beat you in the next roll !!! "
-      // String draw = "Okay, we draw. Let's try this again "
-      // String lose = "You lose, try beat me in your next life, loser !!! "
-      // if (userChoice == "rock") {
-      //   if (choice == "paper") {  
-      //        msg = lose;
-      //        count_computer++;
-      //    }
-      //   else if (choice == "scissor") {
-      //        msg = win;
-      //        count_player++;
-      //    }
-      //   else msg = draw;
-      // }
-      // else if (userChoice == "paper") {
-      //   if (choice == "scissor") {
-      //        msg = lose;
-      //        count_computer++;
-      //    }
-      //   else if (choice == "rock") {
-      //        msg = win;
-      //        count_player++;
-      //    }
-      //   else msg = draw;
-      // }
-      // else {
-      //   if (choice == "rock") {
-      //        msg = lose;
-      //        count_computer++;
-      //    }
-      //   else if (choice == "paper") {
-      //        msg = win
-      //        count_player++;
-      // };
-      //   else msg = draw;
-      // }
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyGameState();
+}
+
+class _MyGameState extends State<MyHomePage> {
+  int count_player = 0;
+  int count_computer = 0;
+  String win = "You win, but i will beat you in the next roll !!! ";
+  String draw = "Okay, we draw. Let's try this again ";
+  String lose = "You lose, try beat me in your next life, loser !!! ";
+  String msg = "Choose an option";
+
+  void selectedOption(String userChoice) {
+    List<String> options = ["rock", "paper", "scissor"];
+    String computerChoice = options[Random().nextInt(3)];
+    setState(() {
+      if (userChoice == "rock") {
+        if (computerChoice == "paper") {
+          msg = lose;
+          count_computer++;
+        } else if (computerChoice == "scissor") {
+          msg = win;
+          count_player++;
+        } else {
+          msg = draw;
+        }
+      } else if (userChoice == "paper") {
+        if (computerChoice == "scissor") {
+          msg = lose;
+          count_computer++;
+        } else if (computerChoice == "rock") {
+          msg = win;
+          count_player++;
+        } else {
+          msg = draw;
+        }
+      } else {
+        if (computerChoice == "rock") {
+          msg = lose;
+          count_computer++;
+        } else if (computerChoice == "paper") {
+          msg = win;
+          count_player++;
+        } else {
+          msg = draw;
+        }
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
