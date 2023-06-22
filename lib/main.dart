8c95149bfc8054dec0b724b872eb5c7a6534cbcf
José Winny <jose.winny.dev@gmail.com>
2023-06-22 01:59:08 -0300

    Update code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
-// import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,12 +7,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    // ignore: prefer_const_constructors
     return MaterialApp(
       title: 'Jokenpo',
-      home: MyHomePage(title: 'Jokepo'),
+      home: MyHomePage(title: 'Jokenpo'),
     );
   }
 }
@@ -29,12 +28,11 @@ class MyHomePage extends StatefulWidget {
 
 class _MyGameState extends State<MyHomePage> {
   int countPlayer = 0;
-  String win = "You win, but i will beat you in the next roll !!! ";
-  String draw = "Okay, we draw. Let's try this again ";
-  String lose = "You lose, try beat me in your next life, loser !!! ";
+  String win = "You win, but I will beat you in the next roll!";
+  String draw = "Okay, we draw. Let's try this again.";
+  String lose = "You lose, try to beat me in your next life, loser!";
   String msg = "Choose an option";
-
-  IconData? get iconComp => null;
+  IconData? iconComp;
 
   void selectedOption(String userChoice) {
     List<String> options = ["rock", "paper", "scissor"];
@@ -45,7 +43,8 @@ class _MyGameState extends State<MyHomePage> {
       Icons.back_hand,
       Icons.cut
     ];
-    IconData iconComp = iconOptions[computerNumber];
+    iconComp = iconOptions[computerNumber];
+
     setState(() {
       if (userChoice == "rock") {
         if (computerChoice == "paper") {
@@ -85,29 +84,33 @@ class _MyGameState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Rock Paper Scissors"),
-        backgroundColor: Colors.deepPurple,
+        title: const Text("Rock Paper Scissors"),
+        backgroundColor: Colors.lightBlue[500],
       ),
       body: Center(
-          child: Stack(
-        children: [
-          Container(
-            color: Color.fromARGB(255, 184, 131, 194),
-          ),
-          Center(
-            child: Stack(children: [
-              Text(
-                msg,
-                style: TextStyle(
-                  fontSize: 20.0,
-                ),
-                textAlign: TextAlign.center,
+        child: Stack(
+          children: [
+            Container(
+              color:Colors.lightBlue[100],
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    msg,
+                    style: const TextStyle(
+                      fontSize: 20.0,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  if (iconComp != null) Icon(iconComp),
+                ],
               ),
-              Icon(iconComp)
-            ]),
-          )
-        ],
-      )),
+            ),
+          ],
+        ),
+      ),
       floatingActionButton: FractionallySizedBox(
         widthFactor: 0.9,
         child: Row(
@@ -133,7 +136,7 @@ class _MyGameState extends State<MyHomePage> {
               }),
               tooltip: 'Scissor',
               child: const Icon(Icons.cut),
-            )
+            ),
           ],
         ),
       ),
